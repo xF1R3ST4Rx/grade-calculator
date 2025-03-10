8945eab01c94880e9fac5b4a21871579556fb822
xF1R3ST4Rx <157852178+xF1R3ST4Rx@users.noreply.github.com>
2025-03-10 10:20:42 -0400

    added functionality that lets the user input non-decimals for grade weights.

--- src/logic.java
+++ src/logic.java
@@ -2,6 +2,12 @@ public class logic {
 
     //method that will execute the grade equation
     public static double add(int grade, int final_exam, double grade_weight, double final_weight){
+        if(grade_weight >= 1){
+            grade_weight = grade_weight/100;
+        }
+        if(final_weight >= 1){
+            final_weight = final_weight/100;
+        }
         double final_grade = (grade_weight * (grade)) + (final_weight * (final_exam));
         return final_grade;
     }
